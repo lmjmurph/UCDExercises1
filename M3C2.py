6ef1ab5a504d35ebdfcf91ff127e52eb9b6f792c
Lisa.Murphy <lisamurphy@gmail.com>
2021-07-31 20:46:32 +0100

    Completed Module 3 Chapter 2 (M2C2)

---
+++ M3C2.py
@@ -0,0 +1,51 @@
+# Import the pandas package
+import pandas as pd
+import numpy as np
+
+# Exercises 1, 2
+
+# Cannot replicate as the categories dataframe is not provided
+
+# Exercise 3
+airlines = pd.read_csv("airlines_final.csv")
+# Print unique values of both columns
+print(airlines.info())
+print(airlines['dest_region'].unique())
+print(airlines['dest_size'].unique())
+
+# Lower dest_region column and then replace "eur" with "europe"
+airlines['dest_region'] = airlines['dest_region'].str.lower()
+airlines['dest_region'] = airlines['dest_region'].replace({'eur':'europe'})
+
+
+# Exercise 4
+
+# Remove white spaces from `dest_size`
+airlines['dest_size'] = airlines['dest_size'].str.strip()
+
+# Verify changes have been effected
+print(airlines['dest_region'].unique())
+print(airlines['dest_size'].unique())
+
+
+# Exercise 5
+# Create ranges for categories
+label_ranges = [0, 60, 180, np.inf]
+label_names = ['short', 'medium', 'long']
+
+# Create wait_type column
+airlines['wait_type'] = pd.cut(airlines["wait_min"], bins = label_ranges,
+                                labels = label_names)
+
+# Create mappings and replace
+mappings = {'Monday':'weekday', 'Tuesday':'weekday', 'Wednesday': 'weekday',
+            'Thursday': 'weekday', 'Friday': 'weekday',
+            'Saturday': 'weekend', 'Sunday': 'weekend'}
+
+airlines['day_week'] = airlines['day'].replace(mappings)
+
+
+# Exercise 6
+
+# Can't replicate as the respondents name column is not in the dataframe
+
